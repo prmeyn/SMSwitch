1ed3d01bb979266a5efb8561fce41f6bc1e36149
tony <ajrmeyn@gmail.com>
2024-06-24 15:25:31 +0200

    critical if none work

--- SMSwitch/SMSwitchService.cs
+++ SMSwitch/SMSwitchService.cs
@@ -1,5 +1,7 @@
 ﻿using HumanLanguages;
+using Microsoft.Extensions.Logging;
 using SMSwitch.Database;
+using SMSwitch.Database.DTOs;
 using SMSwitchCommon;
 using SMSwitchCommon.DTOs;
 using SMSwitchPlivo;
@@ -19,6 +21,7 @@ namespace SMSwitch
 
 		private readonly SMSwitchDbService _smSwitchDbService;
 
+		private readonly ILogger<SMSwitchService> _logger;
 
 
 		public SMSwitchService(
@@ -26,7 +29,8 @@ namespace SMSwitch
 			TwilioService twilioService,
 			TelesignService telesignService,
 			PlivoService plivoService,
-			SMSwitchDbService smSwitchDbService
+			SMSwitchDbService smSwitchDbService,
+			ILogger<SMSwitchService> logger
 			)
 		{
 			_smSwitchInitializer = smSwitchInitializer;
@@ -34,70 +38,82 @@ namespace SMSwitch
 			_telesignService = telesignService;
 			_plivoService = plivoService;
 			_smSwitchDbService = smSwitchDbService;
+			_logger = logger;
 		}
 
 		public async Task<SMSwitchResponseSendOTP> SendOTP(MobileNumber mobileWithCountryCode, HashSet<LanguageIsoCode> preferredLanguageIsoCodeList, UserAgent userAgent)
 		{
-			var expiryTimeUtc = DateTimeOffset.UtcNow.AddSeconds(_smSwitchInitializer.SmsControls.SessionTimeoutInSeconds);
-			var session = await _smSwitchDbService.GetOrCreateAndGetLatestSession(mobileWithCountryCode, expiryTimeUtc);
-
-			Queue<SmsProvider> smsProvidersQueue = null;
-			if (session.SmsProvidersQueue?.Any() ?? false)
-			{
-				smsProvidersQueue = session.SmsProvidersQueue;
-			}
-			else
-			{
-				smsProvidersQueue = new();
-				HashSet<SmsProvider> smsProviders = null;
-				if (!_smSwitchInitializer.SmsControls.PriorityBasedOnCountryPhoneCode.TryGetValue(mobileWithCountryCode.CountryPhoneCodeAsNumericString, out smsProviders))
-				{
-					smsProviders = _smSwitchInitializer.SmsControls.FallBackPriority;
-				}
-				for (int i = 0; i < _smSwitchInitializer.SmsControls.MaxRoundRobinAttempts; i++)
-				{
-					foreach (SmsProvider smsProvider in smsProviders)
-					{
-						smsProvidersQueue.Enqueue(smsProvider);
-					}
-				}
-			}
-
-			if (smsProvidersQueue.Count == 0)
-			{
-				return new SMSwitchResponseSendOTP()
-				{
-					IsSent = false
-				};
-			}
-
-
 			SMSwitchResponseSendOTP responseSendOTP = null;
-
-			while (smsProvidersQueue.Count > 0)
+			SMSwitchSession session = null;
+			try 
 			{
+				var expiryTimeUtc = DateTimeOffset.UtcNow.AddSeconds(_smSwitchInitializer.SmsControls.SessionTimeoutInSeconds);
+				session = await _smSwitchDbService.GetOrCreateAndGetLatestSession(mobileWithCountryCode, expiryTimeUtc);
 
-				responseSendOTP = smsProvidersQueue.Peek() switch
+				Queue<SmsProvider> smsProvidersQueue = null;
+				if (session.SmsProvidersQueue?.Any() ?? false)
 				{
-					SmsProvider.Twilio => await _twilioService.SendOTP(mobileWithCountryCode, preferredLanguageIsoCodeList, userAgent),
-					SmsProvider.Plivo => await _plivoService.SendOTP(mobileWithCountryCode, preferredLanguageIsoCodeList, userAgent),
-					SmsProvider.Telesign => await _telesignService.SendOTP(mobileWithCountryCode, preferredLanguageIsoCodeList, userAgent),
-					_ => throw new NotImplementedException(),
-				};
-
-				if (responseSendOTP.IsSent)
-				{
-					break;
+					smsProvidersQueue = session.SmsProvidersQueue;
 				}
 				else
 				{
-					smsProvidersQueue.Dequeue();
+					smsProvidersQueue = new();
+					HashSet<SmsProvider> smsProviders = null;
+					if (!_smSwitchInitializer.SmsControls.PriorityBasedOnCountryPhoneCode.TryGetValue(mobileWithCountryCode.CountryPhoneCodeAsNumericString, out smsProviders))
+					{
+						smsProviders = _smSwitchInitializer.SmsControls.FallBackPriority;
+					}
+					for (int i = 0; i < _smSwitchInitializer.SmsControls.MaxRoundRobinAttempts; i++)
+					{
+						foreach (SmsProvider smsProvider in smsProviders)
+						{
+							smsProvidersQueue.Enqueue(smsProvider);
+						}
+					}
+				}
+
+				if (smsProvidersQueue.Count == 0)
+				{
+					return new SMSwitchResponseSendOTP()
+					{
+						IsSent = false
+					};
+				}
+
+				while (smsProvidersQueue.Count > 0)
+				{
+
+					responseSendOTP = smsProvidersQueue.Peek() switch
+					{
+						SmsProvider.Twilio => await _twilioService.SendOTP(mobileWithCountryCode, preferredLanguageIsoCodeList, userAgent),
+						SmsProvider.Plivo => await _plivoService.SendOTP(mobileWithCountryCode, preferredLanguageIsoCodeList, userAgent),
+						SmsProvider.Telesign => await _telesignService.SendOTP(mobileWithCountryCode, preferredLanguageIsoCodeList, userAgent),
+						_ => throw new NotImplementedException(),
+					};
+
+					if (responseSendOTP.IsSent)
+					{
+						break;
+					}
+					else
+					{
+						smsProvidersQueue.Dequeue();
+					}
+				}
+
+				session.SmsProvidersQueue = smsProvidersQueue;
+				await _smSwitchDbService.UpdateSession(session);
+
+				if (responseSendOTP == null || !responseSendOTP.IsSent)
+				{
+					_logger.LogCritical($"Unable to send OTP to {mobileWithCountryCode} with SessionId{session?.SessionId}");
 				}
 			}
-
-			session.SmsProvidersQueue = smsProvidersQueue;
-			await _smSwitchDbService.UpdateSession(session);
-
+			catch ( Exception exception) 
+			{
+				_logger.LogCritical(exception, $"Unable to send OTP to {mobileWithCountryCode} with SessionId{session?.SessionId}");
+			}
+			
 			return responseSendOTP ?? new SMSwitchResponseSendOTP() { IsSent = false }; 
 		}
 

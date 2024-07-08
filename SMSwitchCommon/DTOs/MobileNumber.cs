99f5a500963441b3e044565c4e007b841a0dc956
Prashansa Meyn <pm@globalpay.dk>
2024-07-08 23:21:18 +0200

    ignore what is not needed

--- SMSwitchCommon/DTOs/MobileNumber.cs
+++ SMSwitchCommon/DTOs/MobileNumber.cs
@@ -9,11 +9,16 @@ namespace SMSwitchCommon.DTOs
 		[JsonPropertyName("countryIsoCode")] public required string CountryIsoCodeString { get; init; }
 		[JsonPropertyName("countryPhoneCode")] public required string CountryPhoneCode { get; set; }
         [JsonPropertyName("phoneNumber")] public required string PhoneNumber { get; set; }
+
+        [JsonIgnore]
         public CountryIsoCode? CountryIsoCode => Enum.TryParse(CountryIsoCodeString, ignoreCase: true, out CountryIsoCode countryIsoCode) ? countryIsoCode : null;
 		private string removeNonNumericString(string input) => Regex.Replace(input, "[^0-9]", "");
-        public string CountryPhoneCodeAsNumericString => removeNonNumericString(CountryPhoneCode);
+		[JsonIgnore]
+		public string CountryPhoneCodeAsNumericString => removeNonNumericString(CountryPhoneCode);
+		[JsonIgnore] 
         public string PhoneNumberAsNumericString => removeNonNumericString(PhoneNumber);
         private long removeNonNumeric(string input) => long.Parse(removeNonNumericString(input));
+		[JsonIgnore] 
         public string CountryPhoneCodeAndPhoneNumber => $"{removeNonNumeric(CountryPhoneCode)}{removeNonNumeric(PhoneNumber)}";
         public byte PhoneNumberNumericLength() => Convert.ToByte($"{removeNonNumeric(PhoneNumber)}".Length);
 
